c64154bc4f6ad95c6607103d6e1410ccb99d8205
Dmitrii Baranov <me@dmbaranov.io>
2024-05-22 00:21:13 +0300

    Moved int extraction to a separate method

--- src/main/kotlin/parser/Parser.kt
+++ src/main/kotlin/parser/Parser.kt
@@ -4,7 +4,6 @@ import it.skrape.core.htmlDocument
 import it.skrape.fetcher.HttpFetcher
 import it.skrape.fetcher.extractIt
 import it.skrape.fetcher.skrape
-import it.skrape.selects.html5.a
 import it.skrape.selects.html5.div
 import org.steamgifts.giveaway.PartialGiveaway
 import java.util.ArrayList
@@ -13,6 +12,7 @@ class Parser {
     fun getRawGiveawayList(): List<PartialGiveaway> {
         val giveawaysList: List<PartialGiveaway> = skrape(HttpFetcher) {
             request {
+                // TODO: add User-Agent as well, also check for other headers
                 url = "https://steamgifts.com"
             }.also { println("sending the request") }
 
@@ -23,29 +23,17 @@ class Parser {
                         withClass = "giveaway__row-outer-wrap"
                         findAll {
                             forEach { giveawayContainer ->
-                                it.add(
-                                    PartialGiveaway(
-                                        title = giveawayContainer.findFirst(".giveaway__heading__name") {
-                                            text
-                                        },
-                                        url = giveawayContainer.findFirst(".giveaway__heading__name") {
-                                            attribute("href")
-                                        },
-                                        participants = giveawayContainer.findFirst(".giveaway__links") {
-                                            a {
-                                                findFirst {
-                                                    text.filter { it.isDigit() }.toInt()
-                                                }
-                                            }
-                                        },
-                                        price = giveawayContainer.findLast(".giveaway__heading__thin") {
-                                            text.filter { it.isDigit() }.toInt()
-                                        },
-                                        copies = giveawayContainer.findFirst(".giveaway__heading__thin") {
-                                            if (text.contains("Copies")) text.filter { it.isDigit() }.toInt() else 1
-                                        }
-                                    )
+                                val giveaway = PartialGiveaway(
+                                    title = giveawayContainer.findFirst(".giveaway__heading__name") { text },
+                                    url = giveawayContainer.findFirst(".giveaway__heading__name") { attribute("href") },
+                                    participants = giveawayContainer.findFirst(".giveaway__links a") { getIntValue(text) },
+                                    price = giveawayContainer.findLast(".giveaway__heading__thin") { getIntValue(text) },
+                                    copies = giveawayContainer.findFirst(".giveaway__heading__thin") {
+                                        if (text.contains("Copies")) getIntValue(text) else 1
+                                    }
                                 )
+
+                                it.add(giveaway)
                             }
                         }
                     }
@@ -61,4 +49,6 @@ class Parser {
     fun getCurrentPoints(): Int {
         return 0
     }
+
+    private fun getIntValue(str: String): Int = str.filter { it.isDigit() }.toInt()
 }
